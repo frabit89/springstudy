9d77f6a7fa02c2eec8aa04cf03499d66c338fddc
frabit89 <frabit89@gmail.com>
2024-03-13 20:19:02 +0900

    20240313

---
+++ 01_IoC/src/main/java/com/gdu/prj01/anno03/MyController.java
@@ -0,0 +1,17 @@
+package com.gdu.prj01.anno03;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class MyController {
+  
+  private MyService myService;
+  
+  public void add() {
+    myService.add();
+    System.out.println("Controller() add() 호출");
+  }
+  
+}
